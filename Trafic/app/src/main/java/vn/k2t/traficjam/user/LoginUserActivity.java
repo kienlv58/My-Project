6bb1092a2d3fe518b2772cfda3969430984512db
Kien <kienlv@newwayplus.com.vn>
2016-07-06 17:47:20 +0700

    sdf

--- Trafic/app/src/main/java/vn/k2t/traficjam/user/LoginUserActivity.java
+++ Trafic/app/src/main/java/vn/k2t/traficjam/user/LoginUserActivity.java
@@ -1,6 +1,8 @@
 package vn.k2t.traficjam.user;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,6 +19,13 @@ import com.facebook.FacebookSdk;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.firebase.client.Firebase;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.plus.Plus;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
@@ -24,6 +33,7 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import java.util.Arrays;
 
@@ -31,7 +41,7 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 import vn.k2t.traficjam.R;
 
-public class LoginUserActivity extends AppCompatActivity implements View.OnClickListener {
+public class LoginUserActivity extends AppCompatActivity implements View.OnClickListener,GoogleApiClient.OnConnectionFailedListener,GoogleApiClient.ConnectionCallbacks{
 
     @Bind(R.id.edt_email)
     EditText edt_email;
@@ -53,6 +63,8 @@ public class LoginUserActivity extends AppCompatActivity implements View.OnClick
 
     //login facebook
     CallbackManager callbackManager;
+    //login google
+    GoogleApiClient mGoogleApiClient;
 
 
 
@@ -105,6 +117,17 @@ public class LoginUserActivity extends AppCompatActivity implements View.OnClick
             }
         });
 
+        //login google
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.web_app))
+                .requestEmail()
+                .build();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, (GoogleApiClient.OnConnectionFailedListener) this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .addApi(Plus.API)
+                .build();
+
     }
 
     @Override
@@ -114,10 +137,14 @@ public class LoginUserActivity extends AppCompatActivity implements View.OnClick
             case R.id.btn_loginFB:
                 LoginManager.getInstance().logInWithReadPermissions(LoginUserActivity.this, Arrays.asList("public_profile", "email", "user_friends"));
                 break;
+            case R.id.btn_loginGoogle:
+                Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+                startActivityForResult(signInIntent, 100);
+                break;
         }
 
     }
-
+//fb + firebase
     public void handeFBaccesstoken(AccessToken accessToken){
         AuthCredential credential = FacebookAuthProvider.getCredential(accessToken.getToken());
         mAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
@@ -134,6 +161,22 @@ public class LoginUserActivity extends AppCompatActivity implements View.OnClick
 
     }
 
+    //google + firebase
+    public void firebaseAuthWithGoogle(GoogleSignInAccount account){
+        AuthCredential authCredential = GoogleAuthProvider.getCredential(account.getIdToken(),null);
+        mAuth.signInWithCredential(authCredential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(LoginUserActivity.this,task+"",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(LoginUserActivity.this,"login fail",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
     @Override
     protected void onStart() {
         super.onStart();
@@ -143,6 +186,35 @@ public class LoginUserActivity extends AppCompatActivity implements View.OnClick
     @Override
     protected void onStop() {
         super.onStop();
+        if (mAuthStateListener != null )
         mAuth.removeAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode,resultCode,data);
+        if (requestCode == 100){
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()){
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            }
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
